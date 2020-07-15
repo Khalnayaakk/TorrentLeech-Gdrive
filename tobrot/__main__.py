b89c7789b41336749d13034a5e6a944dd9425db9
devillD <53294521+devillD@users.noreply.github.com>
2020-07-15 04:30:03 +0000

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -102,7 +102,7 @@ if __name__ == "__main__" :
     #
     status_message_handler = MessageHandler(
         status_message_f,
-        filters=Filters.command(["status"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["status@trntxlchbot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(status_message_handler)
     #
